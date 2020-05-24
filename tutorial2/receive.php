c4007b93d4e48d62f19331513a622af50825bff2
Gustavo Carvalho <gustavocarvalhogscrj@gmail.com>
2020-05-24 12:39:38 -0300

    Documentando o tutorial 2

--- tutorial2/receive.php
+++ tutorial2/receive.php
@@ -3,13 +3,19 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
+//Abre a conexão com o RabbitMQ
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+
+//Cria um channel
 $channel = $connection->channel();
 
+//Declara uma queue chamad 'hello' caso não exista 
 $channel->queue_declare('hello', false, false, false, false);
 
+//Exibe no console que está escutando a fila
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
+//Cria um callback para receber a mensagem
 $callback = function ($msg) {
     echo ' [x] Received ', $msg->body, "\n";
     sleep(substr_count($msg->body, '.'));
@@ -17,8 +23,14 @@ $callback = function ($msg) {
     $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
   };
   
+//Declara um consumidor que fica escutando a queue "hello";  
 $channel->basic_consume('hello', '', false, false, false, false, $callback);
 
+//Escuta a fila
 while ($channel->is_consuming()) {
     $channel->wait();
 }
+
+//Fecha o channel e a connection
+$channel->close();
+$connection->close();
--- tutorial2/send.php
+++ tutorial2/send.php
@@ -4,17 +4,25 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+//Abre a conexão com o RabbitMQ
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+
+//Cria um channel
 $channel = $connection->channel();
 
+//Declara uma queue caso não exista 
 $channel->queue_declare('hello', false, false, false, false);
 
+//Pega uma string do console
 $data = implode(' ', array_slice($argv, 1));
 if (empty($data)) {
     $data = "Hello World!";
 }
+
+//Cria uma mensagem
 $msg = new AMQPMessage($data);
 
+//Publica a mensagem
 $channel->basic_publish($msg, '', 'hello');
 
 echo ' [x] Sent ', $data, "\n";
